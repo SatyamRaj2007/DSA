832312c06fb396acf1bacbf66a2b7b0a350f0ae9
Satyam Raj <25BCS13064@cuchd.in>
2026-04-30 22:52:00 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (94.40%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        int i = 0, j = 0;
-        int m1 = 0, m2 = 0;
+        
+        int n=nums1.size();
+        int m=nums2.size();
+        int i=0, j=0;
+        int n1=0;
+        int m1=0;
 
-        // Traverse till middle of merged array
-        for (int count = 0; count <= (n + m) / 2; count++) {
-            m2 = m1;
+        for(int a=0;a<=(n+m)/2;a++){
+            m1=n1;
 
-            if (i < n && j < m) {
-                if (nums1[i] <= nums2[j]) {
-                    m1 = nums1[i++];
-                } else {
-                    m1 = nums2[j++];
+            if(i<n && j<m){
+                if(nums1[i]<=nums2[j]){
+                    n1=nums1[i++];
+                }else{
+                    n1=nums2[j++];
                 }
+            }else if(i<n){
+                n1=nums1[i++];
+            }else{
+                n1=nums2[j++];
             }
-            else if (i < n) {
-                m1 = nums1[i++];
-            }
-            else {
-                m1 = nums2[j++];
-            }
+
         }
 
-        // If total length is odd
-        if ((n + m) % 2 == 1) {
-            return (double)m1;
+        if((n+m)%2==1){
+            return(double)n1;
         }
 
-        // If total length is even
-        return (m1 + m2) / 2.0;
+        return (n1+m1)/2.0;
     }
 };
