2a4d2b7c581c64f308a2e96c8bb80b8a552c99a8
Satyam Raj <25BCS13064@cuchd.in>
2026-07-29 12:53:12 +0530

    Time: 25 ms (5.15%) | Memory: 26 MB (5.16%) - LeetSync

---
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        int n = s.size();
+        vector<int> nums;
+        deque<int> dq;
+
+        for(int i = 0; i < n; i++) {
+            nums.push_back(s[i]);
+        }
+
+        int count = 0;
+
+        // First window
+        for(int i = 0; i < k; i++) {
+            dq.push_back(nums[i]);
+
+            if(nums[i] == 97 || nums[i] == 101 ||
+               nums[i] == 105 || nums[i] == 111 ||
+               nums[i] == 117) {
+                count++;
+            }
+        }
+
+        int maxcount = count;
+
+        // Sliding window
+        for(int i = 0; i < n - k; i++) {
+
+            // Remove
+            int removed = dq.front();
+            dq.pop_front();
+
+            if(removed == 97 || removed == 101 ||
+               removed == 105 || removed == 111 ||
+               removed == 117) {
+                count--;
+            }
+
+            // Add
+            int added = nums[i + k];
+            dq.push_back(added);
+
+            if(added == 97 || added == 101 ||
+               added == 105 || added == 111 ||
+               added == 117) {
+                count++;
+            }
+
+            maxcount = max(maxcount, count);
+        }
+
+        return maxcount;
+    }
+};
