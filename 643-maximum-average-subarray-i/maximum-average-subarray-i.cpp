a7f0c9ec102db53e71e20813ab8a2ae150841e1f
Satyam Raj <25BCS13064@cuchd.in>
2026-07-29 10:13:53 +0530

    Time: 3 ms (36.40%) | Memory: 113.7 MB (87.30%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -2,25 +2,17 @@ class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
         int n = nums.size();
-
         double average = 0;
 
-        // First window
         for (int i = 0; i < k; i++) {
             average += (double)nums[i] / k;
         }
-
         double storemax = average;
-
-        // Sliding window
         for (int i = 0; i < n - k; i++) {
-
             double newaverage =
                 average - (double)nums[i] / k
                         + (double)nums[i + k] / k;
-
             storemax = max(storemax, newaverage);
-
             average = newaverage;
         }
 
