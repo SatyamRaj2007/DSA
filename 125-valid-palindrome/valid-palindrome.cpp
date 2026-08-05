0fa9d65b5da2306f3ff7b41f5f3512b6b96d2cb0
Satyam Raj <25BCS13064@cuchd.in>
2026-08-05 19:30:36 +0530

    Time: 2 ms (35.93%) | Memory: 10.7 MB (5.20%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
-        int n=s.size();
         string str1;
-        string str2;
-        for(int i=0;i<n;i++){
-            if(!isalnum(s[i])) continue;
-            if(s[i]>='A'&& s[i]<='Z'){
-                s[i]=s[i]+32;
-            }
-            str1.push_back(s[i]);
+        for(char ch:s){
+            if(!isalnum(ch)) continue;
+            str1.push_back(tolower(ch));
         }
-        str2=str1;
+
+        string str2=str1;
         int start=0;
         int end=str2.size()-1;
         while(start<end){
             swap(str2[start++],str2[end--]);
         }
-
-        if(str2==str1) return true;
-        return false;
+        return str2==str1;
     }
 };
