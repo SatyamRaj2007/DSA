9ae4892ece2eb3344642cab56e547df82aa1398d
Satyam Raj <25BCS13064@cuchd.in>
2026-08-05 19:20:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.20%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    bool isAlphaNum(char ch){
-        if((ch>='0' && ch<='9')||
-        (tolower(ch)>='a'&& tolower(ch)<='z')){
-            return true;
-        }
-
-        return false;
-    }
     bool isPalindrome(string s) {
-        int st=0,end=s.length()-1;
-        while(st<end){
-            if(!isAlphaNum(s[st])){
-                st++; 
-                continue;
-            }
-            if(!isAlphaNum(s[end])){
-                end--; 
-                continue;
-            }
-            if(tolower(s[st])!=tolower(s[end])){
-                return false;
-            }
-
-            st++;
-            end--;
-            
-        }
         
-        return true;
+        int n=s.size();
+        
+        string str1;
+        string str2;
+        for(int i=0;i<n;i++){
+            if(!isalnum(s[i])) continue;
+            if(s[i]>='A'&& s[i]<='Z'){
+                s[i]=s[i]+32;
+            }
+            str1.push_back(s[i]);
+        }
+        str2=str1;
+        int start=0;
+        int end=str2.size()-1;
+        while(start<=end){
+            swap(str2[start++],str2[end--]);
+        }
+
+        if(str2==str1) return true;
+        return false;
     }
 };
