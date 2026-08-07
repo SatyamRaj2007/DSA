8142094fbcc92c9ef72d6ca700f6769987789e62
Satyam Raj <25BCS13064@cuchd.in>
2026-08-07 23:32:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (44.50%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        int n=s.size();
+        string ans="";
 
-        stringstream ss(s);
+        reverse(s.begin(),s.end());
 
-        vector<string> words;
-        string word;
-
-        while (ss >> word) {
-            words.push_back(word);
+        for(int i=0;i<n;i++){
+            while (i < n && s[i] == ' ')
+                i++;
+            string word="";
+            while(i<n && s[i]!=' '){
+                word+=s[i];
+                i++;
+            }
+            reverse(word.begin(),word.end());
+            if (!word.empty()){
+                ans += " " + word;
+            }
         }
 
-        string ans;
-
-        for (int i = words.size() - 1; i >= 0; i--) {
-            ans += words[i];
-
-            if (i != 0)
-                ans += " ";
-        }
-
-        return ans;
+        return ans.substr(1);
     }
 };
