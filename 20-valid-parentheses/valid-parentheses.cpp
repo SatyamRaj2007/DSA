a01b23145c3fb99753c3ef5a5cfce7690a5ae74a
Satyam Raj <25BCS13064@cuchd.in>
2026-08-08 16:00:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (68.35%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int n=s.size();
-        stack<char>st;
-        for(auto ch: s){
-            if((ch=='(') || (ch=='{') || (ch=='[')){
+        stack<char> st;
+
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
                 st.push(ch);
-            }else{
-                if(st.empty()){
+            } else {
+                if (st.empty())
+                    return false;
+
+                if ((ch == ')' && st.top() == '(') ||
+                    (ch == '}' && st.top() == '{') ||
+                    (ch == ']' && st.top() == '[')) {
+                    st.pop();
+                } else {
                     return false;
                 }
-
-            if((ch==')' && st.top()=='(') ||
-               (ch=='}' && st.top()=='{') ||
-               (ch==']' && st.top()=='[')) {
-                st.pop();
-            }else{
-                return false;
-            }
             }
         }
 
