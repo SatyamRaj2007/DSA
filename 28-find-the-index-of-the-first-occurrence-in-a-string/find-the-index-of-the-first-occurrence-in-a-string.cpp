b659e468b2d2bc9e0643b8d430c67283a553fc1b
Satyam Raj <25BCS13064@cuchd.in>
2026-09-19 23:42:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (24.67%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,54 +5,16 @@ public:
         int n = haystack.size();
         int m = needle.size();
 
-        // Build LPS array
-        vector<int> lps(m, 0);
+        for (int i = 0; i <= n - m; i++) {
 
-        int len = 0;
-        int i = 1;
+            int j = 0;
 
-        while (i < m) {
-
-            if (needle[i] == needle[len]) {
-                len++;
-                lps[i] = len;
-                i++;
-            }
-            else {
-
-                if (len != 0) {
-                    len = lps[len - 1];
-                }
-                else {
-                    lps[i] = 0;
-                    i++;
-                }
-            }
-        }
-
-        // KMP Search
-        i = 0;
-        int j = 0;
-
-        while (i < n) {
-
-            if (haystack[i] == needle[j]) {
-                i++;
+            while (j < m && haystack[i + j] == needle[j]) {
                 j++;
             }
 
             if (j == m) {
-                return i - j;
-            }
-
-            else if (i < n && haystack[i] != needle[j]) {
-
-                if (j != 0) {
-                    j = lps[j - 1];
-                }
-                else {
-                    i++;
-                }
+                return i;
             }
         }
 
